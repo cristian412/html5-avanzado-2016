f9e3364b85c7fdd818ee8ed56402bc18ea8ad692
cristian412 <cristianamarillacloss@gmail.com>
2019-10-25 20:52:35 -0300

    listado de personas

--- cargar-personas.php
+++ cargar-personas.php
@@ -11,10 +11,27 @@ function insertar($orden){
   mysqli_close($con);
   return $res;
 }
+function peticion($orden){
+  ########### CONEXION #########################################
+  $user = 'b4924e9e775cc0';
+  $pass = '8f4d7791fef8ca1';
+  $db   = 'heroku_d00b69afc685a2e';
+  $server = 'us-cdbr-iron-east-05.cleardb.net';
+  $con = mysqli_connect($server,$user,$pass,$db);
+  $res = mysqli_query($con, $orden);
+  while ($row = mysqli_fetch_assoc($res)) $li[] = $row;
+  mysqli_free_result($res);
+  mysqli_close($con);
+  return $li;
+}
+$lista = '';
+$re = peticion("Select id_personas, nombre, apellido from personas");
+foreach($re as $value){
+  $lista.='<tr><td>'.$value['id_personas'].'</td><td>'.$value['nombre'].'</td><td>'.$value['apellido'].'</td></tr>';
+}
 
 $res = '<h2>Respuesta: </h2>';
 if( isset($_POST['nombrex']) ){
-
 	$res.= '<pre>';
 	foreach($_POST as $key => $v){
 		$res.= $key.'  --> '.$v.' ';
@@ -23,7 +40,6 @@ if( isset($_POST['nombrex']) ){
 	$nom = $_POST['nombrex'];
 	$ape = $_POST['apellidox'];
 	insertar("INSERT INTO `personas` (`nombre`, `apellido`) VALUES ('{$nom}', '{$ape}')");
-
 }
 
 ?>
@@ -46,6 +62,7 @@ if( isset($_POST['nombrex']) ){
 <h1>Cargar Personas</h1>
 
 <?=$res?>
+
 	
 <form method="post" action="cargar-personas.php">
   <label>NOMBRE</label> <input type="text" name="nombrex" />
@@ -58,5 +75,18 @@ if( isset($_POST['nombrex']) ){
 
 </form>
 
+<table>
+	<thead>
+		<tr>
+			<th>Nombre</th>
+			<th>Apellido</th>
+
+		</tr>
+	</thead>
+	<tbody>
+		<?=$lista?>
+	</tbody>
+</table>
+	
 </body>
 </html>
