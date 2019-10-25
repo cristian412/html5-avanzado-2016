c0d5f7b067cc8c3b6fdc91a0db5316bbf9671ed8
cristian412 <cristianamarillacloss@gmail.com>
2019-10-25 20:17:08 -0300

    Create cargar-personas.php

---
+++ cargar-personas.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<h1>Cargar Personas</h1>
+
+<form method="post" action="cargar-personas">
+  <label>NOMBRE</label> <input type="text" name="nombrex" />
+  <br>
+  <label>APELLIDO</label> <input type="text" name="apellidox" />
+
+  <input type="submit" name="submitx"/>
+
+</form>
+
+</body>
+</html>
