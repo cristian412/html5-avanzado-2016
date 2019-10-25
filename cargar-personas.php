8dd0dea7c2a51e5b47bc3c8c3593ea66708e95d7
cristian412 <cristianamarillacloss@gmail.com>
2019-10-25 20:35:07 -0300

    funcion insertar

--- cargar-personas.php
+++ cargar-personas.php
@@ -1,4 +1,16 @@
 <?php
+function insertar($orden){
+  ########### CONEXION #########################################
+  $user = 'b4924e9e775cc0';
+  $pass = '8f4d7791fef8ca1';
+  $db   = 'heroku_d00b69afc685a2e';
+  $server = 'us-cdbr-iron-east-05.cleardb.net';
+  $con = mysqli_connect($server,$user,$pass,$db);
+  $res = mysqli_query($con, $orden);
+  mysqli_free_result($res);
+  mysqli_close($con);
+  return $res;
+}
 
 $res = '<h2>Respuesta: </h2>';
 if( isset($_POST) ){
@@ -8,6 +20,8 @@ if( isset($_POST) ){
 		$res.= $key.'  --> '.$v.' ';
 	}
 	$res.= '</pre>';
+	
+	insertar("INSERT INTO `personas` (`nombre`, `apellido`) VALUES ('{$nombrex}', '{$apellidox}')");
 
 }
 
