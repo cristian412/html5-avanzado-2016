d913a4128ad38526362548de78f003925214ff66
cristian412 <cristianamarillacloss@gmail.com>
2019-10-26 15:40:00 -0300

    Update cargar-personas.php

--- cargar-personas.php
+++ cargar-personas.php
@@ -1,8 +1,8 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-echo '<pre>';
-print_r($url);
-echo '</pre>';
+# echo '<pre>';
+# print_r($url);
+# echo '</pre>';
 
 function insertar($orden){
   ########### CONEXION #########################################
